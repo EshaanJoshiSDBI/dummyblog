5e2ac96233f904dfb601b8ccf44bd3eebea5e086
EshaanJoshiSDBI <eshaanjoshi713@gmail.com>
2021-07-08 00:03:30 +0530

    search by author's name

--- func.js
+++ func.js
@@ -2,16 +2,17 @@ function search(){
     let input = document.getElementById('search_bar').value.toLowerCase();
     let x = document.getElementsByClassName('art');
     let y = document.getElementsByClassName('head');
-    for(var i = 0; i < x.length; i++){
-        let z = y[i].getElementsByTagName('a')[0]
-        if(z.textContent.toLowerCase().indexOf(input) > -1){
+    for(var i = 0; i < 1000; i++){
+        let z = y[i].getElementsByTagName('a')[0];
+        let a = x[i].getElementsByTagName('p')[0];
+        // let b = x[i].getElementsByClassName('ul');
+        // let c = b[i].getElementsByTagName('li');
+        if((z.textContent.toLowerCase().indexOf(input) > -1) || a.textContent.toLowerCase().indexOf(input) > -1 /*|| (c[i].textContent.toLowerCase().indexOf(input) > -1)*/){
             x[i].style.display ='';
         }
         else{
             x[i].style.display = 'none';
         }
     }
-}
-img.ondblclick =  function lcf(){
-
+//img.ondblclick =  function lcf(){
 }
